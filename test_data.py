1953e7214b557dd6896f5548173980f9093b9375
mohamad <ms.khosravani@gmail.com>
2022-12-14 18:44:31 -0600

    load data for driving

--- test_data.py
+++ test_data.py
@@ -4,12 +4,22 @@ import os
 
 
 
+"""
+'x_f', 'vx_f', 'ax_f', 'x_l', 'ay_f', 'vy_f', 'y_l', 'y_f', 'dt'
+"""
 
 
+def load_data(main_path, file_path):
+    return loadmat(os.path.join(data_path,"data/" + file_path))
 
 data_path = Path(__file__).resolve().parent
-data_path = os.path.join(data_path, "data/human_robot_data_p1_train.mat")
+# data_path = os.path.join(data_path, "data/human_robot_data_p1_train.mat")
+data_processed = load_data(data_path, "k_delay_train_p1.mat")
 
-annots = loadmat(data_path)
-print(annots)
 
+# annots = loadmat(data_path)
+
+print(data_processed.keys())
+print(data_processed["k_delay"])
+
+# '__globals__', 'k_delay'
